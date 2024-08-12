3614326477a4d4f018347345870aeec29833ee22
Alessia Cinelli <cinellialessia@gmail.com>
2024-08-12 11:07:16 +0200

    prove inizio

--- prova.py
+++ prova.py
@@ -1,5 +1,5 @@
-from math import log
-from discrete_event_simulation.rngs import putSeed, random
+from discrete_event_simulation.rngs import putSeed
+from discrete_event_simulation.ssq4 import Exponential
 
 # Parametri della simulazione
 TEMPO_SIMULAZIONE = 5 * 60  # 5 minuti in secondi
@@ -10,16 +10,11 @@ TASSO_SERVIZIO = 1 / (3 * 60)  # 1 job ogni 3 minuti
 arrival_times = []
 service_end_times = []
 
-# Funzione per generare tempi esponenziali
-def expovariate(rate):
-    U = random()  # Genera un numero casuale uniforme tra 0 e 1
-    return -1 / rate * log(1 - U)  # Calcola il valore esponenziale
-
 # Generazione dei tempi di arrivo e di fine servizio
 putSeed(42)
 tempo_corrente = 0
 while tempo_corrente < TEMPO_SIMULAZIONE:
-    tempo_arrivo = expovariate(TASSO_ARRIVO)
+    tempo_arrivo = Exponential(TASSO_ARRIVO)
     tempo_corrente += tempo_arrivo
     if tempo_corrente < TEMPO_SIMULAZIONE:
         arrival_times.append(tempo_corrente)
@@ -27,10 +22,9 @@ while tempo_corrente < TEMPO_SIMULAZIONE:
 # Prendi tutti i tempi di arrivo e calcola i tempi di fine servizio
 tempo_corrente = 0
 for tempo_arrivo in arrival_times:
-    tempo_servizio = expovariate(TASSO_SERVIZIO)
+    tempo_servizio = Exponential(TASSO_SERVIZIO)
     tempo_corrente = max(tempo_arrivo, tempo_corrente) + tempo_servizio
     service_end_times.append(tempo_corrente)
-
 
 # Confronto dei tempi di arrivo e di fine servizio
 with open('tempi_servizio.txt', 'w') as file:
