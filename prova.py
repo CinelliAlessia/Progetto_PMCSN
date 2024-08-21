76000200e1f235b369638e87ddbf4bd576607187
Alessia Cinelli <cinellialessia@gmail.com>
2024-08-21 12:39:59 +0200

    Prova generazione numeri, tempi di servizio e arrivo

--- prova.py
+++ prova.py
@@ -11,35 +11,37 @@ TEMPO_SIMULAZIONE = 5 * 60  # 5 minuti in secondi
 TASSO_ARRIVO = 1 / 60  # 1 job al minuto
 TASSO_SERVIZIO = 1 / (3 * 60)  # 1 job ogni 3 minuti
 
-# Liste per memorizzare i tempi di arrivo e di fine servizio
-arrival_times = []
-service_end_times = []
-
 # Generazione dei tempi di arrivo e di fine servizio
 putSeed(123456789)
-tempo_corrente = 0
-num_job = 0
-with open('tempi_arrivo.txt', 'w') as file:
+
+
+def service_time(num_job):
+    # Prendi tutti i tempi di arrivo e calcola i tempi di fine servizio
+    tempo_corrente = 0
+
+    with open('tempi_arrivo.txt', 'r', newline="") as file_arrivi:
+        file_arrivi.seek(0)
+        with open('tempi_servizio.txt', 'w') as file:
+            for i in range(num_job):
+                tempo_servizio = expovariate(TASSO_SERVIZIO)
+                # somma alla riga i esima del file_arrivi
+                tempo_corrente += tempo_servizio + float(file_arrivi.readline())
+                print(f'{tempo_corrente:.2f}')
+                file.write(f'{tempo_corrente:.2f}\n')
+
+
+def arrival_time():
+    file_arrivi = open('tempi_arrivo.txt', 'w')
+    tempo_corrente = 0
+    num_job = 0
     while tempo_corrente < TEMPO_SIMULAZIONE:
         tempo_arrivo = expovariate(TASSO_ARRIVO)
         tempo_corrente += tempo_arrivo
         if tempo_corrente < TEMPO_SIMULAZIONE:
-            file.write(f'{tempo_arrivo:.2f}\n')
+            file_arrivi.write(f'{tempo_corrente:.2f}\n')
             num_job += 1
+    file_arrivi.close()
+    return num_job
 
-
-# Prendi tutti i tempi di arrivo e calcola i tempi di fine servizio
-tempo_corrente = 0
-with open('tempi_servizio.txt', 'w') as file:
-    for tempo_arrivo in range(num_job):
-        tempo_servizio = expovariate(TASSO_SERVIZIO)
-        file.write(f'{tempo_servizio:.2f}\n')
-
-i = 0
-# Confronto dei tempi di arrivo e di fine servizio
-with open('tempi.txt', 'w') as file:
-    for i in range(num_job):
-        if i < len(service_end_times):
-            file.write(f'Job {i+1}: Arrivo = {arrival_times[i]:.2f}, Fine Servizio = {service_end_times[i]:.2f}\n')
-        else:
-            file.write(f'Job {i+1}: Arrivo = {arrival_times[i]:.2f}, Fine Servizio = Non Servito\n')
+num_job = arrival_time()
+service_time(num_job)
