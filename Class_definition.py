2ffd343f6f1e54d9a4bf91bb9a58d508f83d3956
andreatozzi99 <andreatozzi199@gmail.com>
2024-09-03 09:49:01 +0200

    Update initial structure

--- Class_definition.py
+++ Class_definition.py
@@ -2,23 +2,23 @@ from Costant import *
 
 class Times:
     def __init__(self):
-        self.current = 0  # System clock - Avanzamento del tempo solo all'occorrenza di un next event
-        self.next = 0  # Occurrence of the next event
-        self.last = [0 for _ in range(QUEUES_NUM)]  # Last arrival time for each flow - NUMBER_OF_QUEUES elements
+        self.current = 0
+        self.next = 0  # Tempo del prossimo evento
+        self.last = [0 for _ in range(QUEUES_NUM)]  # L'ultimo evento di arrivo per ogni classe di utenza
 
 
 class EventList:
     def __init__(self):
-        self.arrivals = []  # Arrival events list, each element is a type of operation
-        self.completed = [Event('C') for _ in range(SERVER_NUM)]  # Completed events list
-        self.sampling = 0  # Sampling event
+        self.arrivals = []  # Lista degli eventi di arrivo, uno per ogni classe di utenza
+        self.completed = [Event('C') for _ in range(SERVER_NUM)]  # Lista degli eventi di completamento
+        self.sampling = 0  # Evento di campionamento
 
 
 class Event:
     def __init__(self, event_type=None):
-        self.event_time = None          # Event time
+        self.event_time = None          # Tempo in cui l'evento si verificherà
         self.event_type = event_type    # Arrivo o Completamento (A o C)
-        self.op_index = None            # Operation Index - Tipo di operazione - Da 0 a 7
+        self.op_index = None            # Tipo di operazione
         self.serving_time = None        # Tempo di servizio
 
 
